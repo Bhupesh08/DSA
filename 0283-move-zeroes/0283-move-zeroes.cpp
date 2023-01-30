5a814524b53ee1276d7cfc769003e4813ec4c5ea
Bhupesh Bansal <83157814+Bhupesh08@users.noreply.github.com>
2023-01-30 22:58:02 +0530

    Time: 24 ms (77.60%), Space: 19.2 MB (63.45%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        int l = 0; 
+        int n = nums.size();
+        
+        //look how putting l < n after the first condition results in run time error
+        while( l < n && nums[l] != 0) {
+            l++;
+        }
+
+        int r = l + 1;
+
+        while(r<n) {
+            if(nums[r] != 0) {
+                swap(nums[r], nums[l]);
+                r++;
+                l++;
+            } else r++;
+        }
+
+        
+        // for(int r= l+1; r<n; r++) {
+        //     if(nums[r] != 0) {
+        //         swap(nums[r], nums[l]);
+        //         l++;
+        //     }
+        // }
+}
+
+};
