e468ed36f462cb6e2f4ed630275d3e6f4ef63a2a
Bhupesh Bansal <83157814+Bhupesh08@users.noreply.github.com>
2023-02-06 23:11:42 +0530

    Update 0169-majority-element.cpp

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,22 +3,11 @@ public:
     int majorityElement(vector<int>& nums) {
 
         int n = nums.size();
-//         int res;
-//         map<int, int> mpp;
 
-//         for(int i=0; i<n; i++) {
-//             mpp[nums[i]]++;
-//         }
-
-//         for(auto it=mpp.begin(); it!=mpp.end(); it++) {
-//             if(it->second > n/2) {
-//                 res = it->first;
-//             }
-//         }
-
-//         return res;
-        
-        
+        //Most optimal solution using Boyer-Moore Voting algorithm.
+        //Remember it using a game. Candidate is the protaganist (kinda our majority element in the end) and as soon as an element i.e not our candidate
+        //is found, the life of candidate gets decremented, and we know that majority elements appears more than half times, so it will somehow be alive at 
+        //the end as we only decrement the life when any minority element is found.
         int candidate = INT_MIN;
         int life = 0;
         
@@ -38,3 +27,20 @@ public:
         return candidate;
     }
 };
+
+// Brute force se thoda better solution using Hashmap.
+//         int res;
+//         map<int, int> mpp;
+
+//         for(int i=0; i<n; i++) {
+//             mpp[nums[i]]++;
+//         }
+
+//         for(auto it=mpp.begin(); it!=mpp.end(); it++) {
+//             if(it->second > n/2) {
+//                 res = it->first;
+//             }
+//         }
+
+//         return res;
+        
