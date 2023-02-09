3ddddaea146cf06d1ac4b1ab1af743f474c30001
Bhupesh Bansal <83157814+Bhupesh08@users.noreply.github.com>
2023-02-10 00:23:00 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (35.19%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        //method - 1: merge nums2 with nums1 and then sort nums1
+//         int j = 0;
+//         for(int i = m; i<m+n; i++) {
+//             swap(nums1[i], nums2[j]);
+//             j++;
+//         }
+        
+//         sort(nums1.begin(), nums1.end());
+        
+        int i = (m==0) ? 0 : m-1 ;
+        int j = n-1;
+        int k = m+n-1;
+        
+        if(i==0 && m==0) {
+            while(j>=0) {
+                swap(nums2[j], nums1[k]);
+                k--;
+                j--;
+            }
+            
+            return;
+        }
+        
+        while(i >= 0 && j >= 0) {
+            
+            if(nums2[j] > nums1[i]) {
+                swap(nums2[j], nums1[k]);
+                k--;
+                j--;
+            } 
+            
+            else if(nums1[i] > nums2[j]) {
+                swap(nums1[i], nums1[k]);
+                k--;
+                i--;
+                //taking care of the edge case:
+                if(i<0) {
+                    while(j>=0) {
+                        swap(nums2[j], nums1[k]);
+                        k--;
+                        j--;
+                    }
+                }
+            }
+            
+            else{
+                swap(nums1[i], nums1[k]);
+                swap(nums1[i], nums2[j]);
+                j--; k--;
+            }
+        
+        }
+            
+            
+        
+    }
+};
