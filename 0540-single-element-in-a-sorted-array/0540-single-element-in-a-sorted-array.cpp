36998ea0197b55abe7e38e80b5477fd1095b795b
Bhupesh Bansal <83157814+Bhupesh08@users.noreply.github.com>
2023-02-14 19:51:20 +0530

    Time: 32 ms (46.73%), Space: 22.3 MB (62.44%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -13,29 +13,40 @@ public:
         while(r>l+1) {
             int mid = l - (l-r)/2;
             
+            //edge case when mid is pointing to the 0th index
             if(mid-1<0 && nums[mid] < nums[mid+1]) {
                 return nums[mid];
             }
             
+            //edge case when mid is pointing to the (n-1)th index
             if(mid+1==n && nums[mid] > nums[mid-1]) {
                 return nums[mid];
             }
             
+            //Main condition which is to be satisfied for mid to be our actual answer
             if(nums[mid] > nums[mid-1] && nums[mid] < nums[mid+1]) {
                 return nums[mid];
             }
             
+            //Case A: When element (where mid is pointing) is the second occurrence of that element
             else if(nums[mid]==nums[mid-1]) {
+                //if second occurrence present at odd index: Distortion abhi tak hua nahi hai and
+                //the desired element is on the right side
                 if(mid % 2 == 1) {
                     l = mid;
+                //else desired element is on the left side
                 } else {
                     r = mid;
                 }
             }
             
+            //Case B: When element (where mid is pointing) is the first occurrence of that element
             else if(nums[mid]==nums[mid+1]) {
+                //if first occurrence present at even index: Distortion abhi tak hua nahi hai and
+                //the desired element is on the right side
                 if(mid % 2 == 0) {
                     l = mid;
+                //else desired element is on the left side
                 } else {
                     r = mid;
                 }
