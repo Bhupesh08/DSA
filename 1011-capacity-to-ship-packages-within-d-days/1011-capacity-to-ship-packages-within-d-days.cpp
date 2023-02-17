2bea2123b56df31d5aae7fd4854db189de19eda8
Bhupesh Bansal <83157814+Bhupesh08@users.noreply.github.com>
2023-02-17 22:46:17 +0530

    Time: 71 ms (49.94%), Space: 26 MB (95.97%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,75 @@
+class Solution {
+    
+private:
+    bool good(vector<int>& weights, int days, int capacity) {
+        int n = weights.size();
+        
+        int curWeight = 0;
+        int daysTaken = 0;
+        
+        for(int i=0; i<n; i++) {
+            if(curWeight < capacity) {
+                
+                curWeight += weights[i];
+                
+                if(curWeight==capacity) {
+                    daysTaken++;
+                    curWeight = 0;
+                }
+                
+                if(curWeight > capacity) {
+                    daysTaken++;
+                    curWeight = weights[i];
+                }
+            }
+            
+        }
+        
+        if(curWeight > 0) {
+            daysTaken++;
+        }
+        
+        if(daysTaken <= days) {
+            return true;
+        } 
+        
+        return false;
+        
+    }
+    
+    
+    
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        
+        int n = weights.size();
+        
+        int sum = 0, maxi = INT_MIN;
+        for(int i=0; i<n; i++) {
+            sum += weights[i];
+            if(weights[i] > maxi) {
+                maxi = weights[i];
+            }
+        }
+        
+        
+        
+        int l = maxi - 1;
+        int r = sum + 1;
+        
+        while(r > l+1) {
+            int mid = l - (l-r)/2;
+            
+            if(good(weights, days, mid)) {
+                r = mid;
+            } else {
+                l = mid;
+            }
+        }
+        
+        return r;
+       
+        
+        
+    }
+};
