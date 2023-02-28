ebd0bdaa6a79232c7e4d635da9885d0b25e1da2f
Bhupesh Bansal <83157814+Bhupesh08@users.noreply.github.com>
2023-03-01 03:08:52 +0530

    Time: 23 ms (77.79%), Space: 15.2 MB (21.95%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -12,6 +12,7 @@ class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
         
+        //To count the no of occurrences of the node with value val, it will be later usefull as we will know how much times we have to execute the deletion node method
         int count = 0;
         ListNode* temp = head;
         while(temp!=NULL) {
@@ -21,6 +22,7 @@ public:
             temp = temp->next; 
         }
         
+        //To take care of the cases like [7,7,7,3,4] || [8,8,8,8], cases likes these needs to be handled separately.
         while(head!=NULL && head->val==val) {
             temp = head;
             head = head->next;
@@ -28,6 +30,7 @@ public:
             count--;
         }
         
+        //The main part
         while(count--) {
             ListNode* prev = head;
             ListNode* curr = head;
