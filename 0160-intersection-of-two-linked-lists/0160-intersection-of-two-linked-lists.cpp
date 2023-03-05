497a459185f5b5446a91a9b61aa3c9695d453778
Bhupesh Bansal <83157814+Bhupesh08@users.noreply.github.com>
2023-03-06 02:31:16 +0530

    Update 0160-intersection-of-two-linked-lists.cpp

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -14,18 +14,21 @@ public:
         ListNode* dummyA = headA;
         ListNode* dummyB = headB;
         
+        //Counting the number of Nodes in List A
         while(dummyA!=NULL) {
             countA++;
             dummyA = dummyA->next;
         }
-        
+        //Counting the number of Nodes in List B
         while(dummyB!=NULL) {
             countB++;
             dummyB = dummyB->next;
         }
         
+        //offset is the difference between the length of both the lists
         int offset = abs(countA - countB);
         
+        //Longer List ko utne ka headstart dedo jitna difference hai
         if(countA > countB) {
             while(offset) {
                 headA = headA->next;
@@ -38,6 +41,8 @@ public:
             }
         }
         
+        //Headstart de diya?? Haan! Badiya
+        //Ab dono ko sath me chalayo, if at some moment they point to same node, return that node as it is the intersection point
         while(headA!=NULL && headB!=NULL) {
             if(headA==headB) return headA;
             else {
