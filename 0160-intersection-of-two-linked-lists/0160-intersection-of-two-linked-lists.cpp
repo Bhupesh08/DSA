8abb4fc7b543f57808848f6a6155d7e4f3c659a0
Bhupesh Bansal <83157814+Bhupesh08@users.noreply.github.com>
2023-03-06 01:57:48 +0530

    Time: 76 ms (23.72%), Space: 18 MB (9.21%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -11,11 +11,13 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         set<ListNode*> hashset;
         
+        //Storing the address of all the nodes of A LinkedList
         while(headA!=NULL) {
             hashset.insert(headA);
             headA = headA->next;
         }
         
+        //Traversing the B LinkedList, If any address of A is found, means that is the intersection
         while(headB!=NULL) {
             if(hashset.count(headB)) {
                 return headB;
