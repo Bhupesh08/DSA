d6365ba3bb6a49dfae5bb6c94a41d89a5b9d0db2
Bhupesh Bansal <83157814+Bhupesh08@users.noreply.github.com>
2023-03-24 00:16:35 +0530

    Time: 8 ms (86.32%), Space: 21.3 MB (76.65%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -12,17 +12,24 @@
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
+        //Base case, return false because we moved further the leaf node and target was not achieved.
         if(root == NULL) return false;
         
+        //decrease the value of targetsum for the next node
         targetSum -= root->val;
         
+        //If the  node at which we are is the leaf node and the targetSum has become zero
+        //Return true as answer has been found
         if(root->left == NULL && root->right == NULL && targetSum == 0) return true;
         
+        //Check the leftSubTree and rightSubTree for the node for the node
         bool leftPath = hasPathSum(root->left, targetSum);
         bool rightPath = hasPathSum(root->right, targetSum);
         
+        //remove the current node's value
         targetSum += root->val;
         
+        //OR because we only need one path, so one true is enough to return the result as TRUE
         return leftPath || rightPath;
     }
 };
