cfbc52366e9868f874236814f42705e4ebda03dd
Bhupesh Bansal <83157814+Bhupesh08@users.noreply.github.com>
2023-04-04 01:20:57 +0530

    Time: 32 ms (15.29%), Space: 6.8 MB (98.89%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int> v(26);
-        int i=0, j=0;
+        vector<int> v(26);   //26 because we want to store 26 uppercase alphabets
+        int i=0, j=0;   
         int res = 0;
         
         while(j < s.size()) {
+            //Calculation: we are storing the frequency of a particular alphabet
             v[s[j] - 'A']++;
             
+            //The number of elements we would like to replace = size of the window - the most frequently occuring element
+            //Condition: (The number of elements we would like to replace <= allowed replacements)
             if((j-i+1) - *max_element(v.begin(), v.end()) <= k) {
                 res = max(res, j-i+1);
                 j++;
