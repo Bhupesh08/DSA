93b1a644980703e5554caac34f47cbfbdcc45c49
Bhupesh Bansal <83157814+Bhupesh08@users.noreply.github.com>
2023-04-04 22:26:01 +0530

    Time: 112 ms (6.06%), Space: 24.8 MB (7.43%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,49 +1,53 @@
 class Solution {
-// private:
-//     int atMost(vector<int> nums, int k) {
+private:
+    int atMost(vector<int> arr, int k) {
         
-//         int res = 0; 
-//         int i = 0, j = 0;
-        
-//         vector<int> freq(3);
-        
-//         while(j < nums.size()) {
-            
-//             k -= freq[nums[j]]++ == 0 ? 1 : 0;
-            
-//             while (k < 0) {
-//                 k += freq[nums[i++]]-- == 1 ? 1 : 0;
-//             }
-            
-//             res += j - i + 1;
-//             j++;
-//         }
-        
-        
-//         return res;
-//     }
+        int i = 0, j = 0;
+        int res = 0;
+        map<int, int> hashmap;
+
+        while(j < arr.size()) {
+            hashmap[arr[j]]++;
+
+            while(hashmap.size() > k) {
+                hashmap[arr[i]]--;
+                if(hashmap[arr[i]] == 0) hashmap.erase(arr[i]);
+                i++;
+            }
+
+            res += j-i+1;
+            j++;
+        } 
+
+        return res;
+    }
 
 public:
     int numberOfSubstrings(string s) {
         
-//         vector<int> arr(s.size());
-//         for (int i = 0; i < s.length(); i++) {
-//             arr[i] = s[i] - 'a';
-//         }
-        
-//         return atMost(arr, 3) - atMost(arr, 2);
-        
-        int left = 0, right = 0, count = 0;
-    unordered_map<char, int> freq;
-    while (right < s.size()) {
-        freq[s[right]]++;
-        right++;
-        while (freq['a'] && freq['b'] && freq['c']) {
-            count += s.size() - right + 1;
-            freq[s[left]]--;
-            left++;
+        vector<int> arr(s.size());
+        for (int i = 0; i < s.length(); i++) {
+            arr[i] = s[i] - 'a';
         }
-    }
-    return count;
+        
+        return atMost(arr, 3) - atMost(arr, 2);
+        
+        
     }
 };
+
+
+
+// int left = 0, right = 0, count = 0;
+//     unordered_map<char, int> freq;
+//     while (right < s.size()) {
+//         freq[s[right]]++;
+//         right++;
+//         while (freq['a'] && freq['b'] && freq['c']) {
+//             cout << s.size() - right + 1 << endl;
+//             count += s.size() - right + 1;
+//             freq[s[left]]--;
+//             left++;
+//         }
+//     }
+//     return count;
