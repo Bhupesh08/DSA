153fdf5737c9fe237fa019cf023b191a672bf811
Bhupesh Bansal <83157814+Bhupesh08@users.noreply.github.com>
2023-04-04 19:53:20 +0530

    Update 0992-subarrays-with-k-different-integers.cpp

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -8,13 +8,17 @@ int atMostkDifferentIntegers(vector<int> &arr, int k) {
 
     while(j < arr.size()) {
         hashmap[arr[j]]++;
-
+        
+        //hashmap.size() gives us the value of the number of unique integers.
         while(hashmap.size() > k) {
             hashmap[arr[i]]--;
             if(hashmap[arr[i]] == 0) hashmap.erase(arr[i]);
             i++;
         }
-
+        
+        //Now we have got us a nice subarray which is a ideal one.
+        //out of that ideal subarray, we can now have (j-i+1) ideal subarrays.
+        //Notice how the result is incremented by the size of the window.
         res += j-i+1;
         j++;
     } 
