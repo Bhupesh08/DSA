92eae9f4746a6f489c9ffa3a1cf0b16b71b63aad
Bhupesh Bansal <83157814+Bhupesh08@users.noreply.github.com>
2023-04-21 23:28:23 +0530

    Time: 23 ms (20.64%), Space: 22.1 MB (5.24%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -7,14 +7,16 @@ public:
         vector<int> max_left;
         vector<int> max_right(n);
         
+        //To get the tower with the highest height in left subarray
         int maxi_left = 0;
         for(int i=0; i<n; i++) {
             max_left.push_back(maxi_left);
             
             maxi_left = max(maxi_left, height[i]);
         }
-        //for(int x: max_left) cout << x << " ";
         
+    
+        //To get the tower with the highest height in right subarray
         int maxi_right = 0;
         for(int i=n-1; i>=0; i--) {
             max_right[i] = maxi_right;
@@ -22,16 +24,16 @@ public:
             maxi_right = max(maxi_right, height[i]);
         }
         
-        //for(int x: max_right) cout << x << " "; 
-        
+     
+        //To get the water stored by index
         vector<int> v;
-        
         for(int i=0; i<n; i++) {
             v.push_back(min(max_left[i], max_right[i])-height[i]);    
         }
         
         int res = 0;
         for(int i=0; i<n; i++) {
+            //Only consider when the amount of water > 0
             if(v[i] > 0) res += v[i];
         }
         
