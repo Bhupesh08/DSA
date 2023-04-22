be5259a24e5177aaecff19d810f3fc49597c7f16
Bhupesh Bansal <83157814+Bhupesh08@users.noreply.github.com>
2023-04-22 23:50:17 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -6,33 +6,31 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
-    private:
-    int helper(int W, int wt[], int val[], int n, vector<vector<int>>& t) {
-        if(W==0 || n==0) return 0;
-        
-        if(t[n][W] != -1) return t[n][W];
-        
-        if(wt[n-1] <= W) {
-            t[n][W] = max(val[n-1] + helper(W-wt[n-1], wt, val, n-1, t), helper(W, wt, val, n-1, t));
-            return t[n][W];
-        }
-        
-        else {
-            t[n][W] = helper(W, wt, val, n-1, t);
-            return t[n][W];
-        }
-    }
-    
     public:
     //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
     {
-        // int t[n+1][W+1];
-        // memset(t, -1, sizeof(t));
+        vector<vector<int>> t(n+1, vector<int>(W+1));
         
-        vector<vector<int>> t(n+1, vector<int>(W+1, -1));
+        //Initialisation with the help of base condition of recursive code.
+        for(int i=0; i<n+1; i++) {
+            for(int j=0; j<W+1; j++) {
+                if(i==0 || j==0) t[i][j] = 0;
+            }
+        }
         
-        return helper(W, wt, val, n, t);
+        //Now the main tabulation starts:
+        for(int i=1; i<n+1; i++) {
+            for(int j=1; j<W+1; j++) {
+                if(wt[i-1] <= j) {
+                    t[i][j] = max(val[i-1] + t[i-1][j-wt[i-1]], t[i-1][j]);
+                } else {
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        
+        return t[n][W];
     }
 };
 
